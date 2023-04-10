d874b312046c866bba75ce8d3ba1af4270bd58d9
somu3341 <somashekarvsv1996@gmail.com>
2023-04-10 16:03:23 +0530

    [somu]Add:Refactor-throw custom exceptions for Invalid User Details.

--- UserRegistartion/User.cs
+++ UserRegistartion/User.cs
@@ -4,50 +4,79 @@ namespace UserRegistartion
 {
     public class User
     {
+        string message;
+        public User (string message)
+        {
+            this.message = message;
+        }
         public static string USER_NAME = "^[A-Z]{1}[a-z]{2,}";
         public static string EMAIL = "^[A-Za-z]{3,}[.]{0,}[a-zA-Z]{3,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
         public static string MOBILE = "^[0-9]{2}\\s[6-9]{1}[0-9]{9}$";
         public static string PASSWORD = "^[A-Za-z]{4,}[!+@+#+$+%+^+&+*+(+)+_+++~+.+,]{1,}[0-9]{3,}$";
-        public static string ALLEMAIL = "^[a-zA-Z]+[.+_]{1}[a-zA-Z]+[@]{1}[a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
-        public string UserFirst(string name)
+        public string UserFirst()
         {
-            if (Regex.IsMatch(name, USER_NAME))
-                return "Valid";
-            return "InValid";
-        }
-        public string UserLast(string name)
-        {
-            if (Regex.IsMatch(name, USER_NAME))
-                return "Valid";
-            return "InValid";
-        }
-        public string Email(string email)
-        {
-            if (Regex.IsMatch(email, EMAIL))
-                return "Valid";
-            return "InValid";
-        }
-        public string Mobile(string mobile)
-        {
-            if (Regex.IsMatch(mobile, MOBILE))
-                return "Valid";
-            return "InValid";
-        }
-        public string Password(string password)
-        {
-            if (Regex.IsMatch(password, PASSWORD))
-                return "Valid";
-            return "InValid";
-        }
-        public string UserAllEmail(string[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
+            try
             {
-                string word = arr[i];
-                if (Regex.IsMatch(word, ALLEMAIL))
+                if (Regex.IsMatch(message, USER_NAME))
                     return "Valid";
+                return "InValid";
             }
-            return "InValid";
+            catch(UserException)
+            {
+                throw new UserException(UserException.ExceptionType.INVALID, "Message is InValid");
+            }
         }
+        public string UserLast()
+        {
+            try
+            {
+                if (Regex.IsMatch(message, USER_NAME))
+                    return "InValid";
+                return "Valid";
+            }
+            catch (UserException)
+            {
+                throw new UserException(UserException.ExceptionType.INVALID, "Message is InValid");
+            }
+        }
+        public string Email()
+        {
+            try
+            {
+                if (Regex.IsMatch(message, EMAIL))
+                    return "Valid";
+                return "InValid";
+            }
+            catch (UserException)
+            {
+                throw new UserException(UserException.ExceptionType.INVALID, "Message is InValid");
+            }
+        }
+        public string Mobile()
+        {
+            try
+            {
+                if (Regex.IsMatch(message, MOBILE))
+                    return "Valid";
+                return "InValid";
+            }
+            catch (UserException)
+            {
+                throw new UserException(UserException.ExceptionType.INVALID, "Message is InValid");
+            }
+        }
+        public string Password()
+        {
+            try
+            {
+                if (Regex.IsMatch(message, PASSWORD))
+                    return "Valid";
+                return "InValid";
+            }
+            catch (UserException)
+            {
+                throw new UserException(UserException.ExceptionType.INVALID, "Message is InValid");
+            }
+        }       
     }
 }
---
+++ UserRegistartion/UserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistartion
+{
+    public class UserException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID
+        }
+        private ExceptionType exceptionType;
+
+        public UserException(ExceptionType type, string message) : base(message)
+        {
+            this.exceptionType = type;
+        }
+    }
+}
+
